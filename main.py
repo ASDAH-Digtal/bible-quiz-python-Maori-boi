cc2d4fc4c36e892b8f69dcaac662ac7173688d79
Maoriboi <78454107+Maori-boi@users.noreply.github.com>
2021-09-29 02:01:31 +0000

    Made Each question layout

---
+++ main.py
@@ -0,0 +1,159 @@
+print("=========================================")
+print("Welcome to Micah's Bible Quiz!")
+print("=========================================")
+
+while True:
+  question_one = input("Where in the Bible does the 'Beast' come out of the sea?\n (a) Colossians\n (b) Hebrews\n (c) John\n (d) Revelation\n>>")
+
+  correct_q1 = ["d","(d)","Revelation"]
+  incorrect_q1 = ["a","(a)","Colossians","b","(b)","Hebrews","c","(c)","John"]
+
+  if question_one.lower() in correct_q1:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_one.lower() in incorrect_q1:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+
+while True:
+  question_two = input("How many books are in the New Testament?\n (a) 27\n (b) 33\n (c) 39\n (d) 28\n>>")
+
+  correct_q2 = ["a","(a)","27"]
+  incorrect_q2 = ["b","(b)","33","c","(c)","39","d","(d)","28"]
+
+  if question_two.lower() in correct_q2:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_two.lower() in incorrect_q2:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+
+while True:
+  question_three = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q3 = ["","()",""]
+  incorrect_q3 = ["","()","","","()","","","()",""]
+
+  if question_three.lower() in correct_q3:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_three.lower() in incorrect_q3:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_four = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q4 = ["","()",""]
+  incorrect_q4 = ["","()","","","()","","","()",""]
+
+  if question_four.lower() in correct_q4:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_four.lower() in incorrect_q4:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_five = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q5 = ["","()",""]
+  incorrect_q5 = ["","()","","","()","","","()",""]
+
+  if question_five.lower() in correct_q5:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_five.lower() in incorrect_q5:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_six = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q6 = ["","()",""]
+  incorrect_q6 = ["","()","","","()","","","()",""]
+
+  if question_six.lower() in correct_q6:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_six.lower() in incorrect_q6:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_seven = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q7 = ["","()",""]
+  incorrect_q7 = ["","()","","","()","","","()",""]
+
+  if question_seven.lower() in correct_q7:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_seven.lower() in incorrect_q7:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_eight = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q8 = ["","()",""]
+  incorrect_q8 = ["","()","","","()","","","()",""]
+
+  if question_eight.lower() in correct_q8:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_eight.lower() in incorrect_q8:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_nine = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q9 = ["","()",""]
+  incorrect_q9 = ["","()","","","()","","","()",""]
+
+  if question_nine.lower() in correct_q9:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_nine.lower() in incorrect_q9:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+while True:
+  question_ten = input("?\n (a) \n (b) \n (c) \n (d) \n>>")
+
+  correct_q10 = ["","()",""]
+  incorrect_q10 = ["","()","","","()","","","()",""]
+
+  if question_ten.lower() in correct_q10:
+    print("Great Job, you got the answer right.")
+    break
+  elif question_ten.lower() in incorrect_q10:
+    print("Not the answer i'm looking for. Nice try.")
+    break
+  else:
+    print("You did not choose one of the options")
+
+print("Thank you for taking part in Micah's Bible Quiz!")
+
+result = input("Would you like to see your score")
